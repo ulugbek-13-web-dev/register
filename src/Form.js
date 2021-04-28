6082051ba7399da623e4a60f7a0a0841bf676c08
SmartCode-site <git config --global user.email web_developer13@mail.ru>
2021-04-29 02:22:23 +0500

    first commit

---
+++ src/Form.js
@@ -0,0 +1,29 @@
+import React, { useState } from 'react';
+import './Form.css';
+import FormSignup from './FormSignup';
+import FormSuccess from './FormSuccess';
+
+const Form = () => {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  function submitForm() {
+    setIsSubmitted(true);
+  }
+  return (
+    <>
+      <div className='form-container'>
+        <span className='close-btn'>×</span>
+        <div className='form-content-left'>
+          <img className='form-img' src='https://raw.githubusercontent.com/briancodex/react-form-v1/286f4a4603bda257ae001dc57c74d7f30bd4eedb/public/img/img-2.svg' alt='spaceship' />
+        </div>
+        {!isSubmitted ? (
+          <FormSignup submitForm={submitForm} />
+        ) : (
+          <FormSuccess />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default Form;
---
+++ src/FormSuccess.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Form.css';
+
+const FormSuccess = () => {
+  return (
+    <div className='form-content-right'>
+      <h1 className='form-success'>We have received your request!</h1>
+      <img className='form-img-2' src='https://raw.githubusercontent.com/briancodex/react-form-v1/286f4a4603bda257ae001dc57c74d7f30bd4eedb/public/img/img-3.svg' alt='success-image' />
+    </div>
+  );
+};
+
+export default FormSuccess;
